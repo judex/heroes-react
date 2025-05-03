dc4f86a8f3921665604d31994c1dcd782108fde0
msn.dproyect <msn.dproyect@gmail.com>
2025-05-03 10:35:26 -0500

    Agregando configuracion para vite para abrir el navegador por defecto vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  server: {
+    open: true, // Abre el navegador automáticamente
+  },  
 })
